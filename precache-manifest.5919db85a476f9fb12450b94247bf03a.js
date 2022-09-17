da269e623c76bd29a1a138a45048ac50221433e1
Vaneet Chanana <vaneetchanana98@gmail.com>
2022-09-18 01:17:59 +0530

    Updates

---
+++ precache-manifest.5919db85a476f9fb12450b94247bf03a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4e2af6070e79fb7a24954597c86d44",
+    "url": "/image-compressor/index.html"
+  },
+  {
+    "revision": "9dfcaea4e09a40b64765",
+    "url": "/image-compressor/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9dfcaea4e09a40b64765",
+    "url": "/image-compressor/static/js/2.69fef3bc.chunk.js"
+  },
+  {
+    "revision": "57c7ec43fbd73feffaae",
+    "url": "/image-compressor/static/js/main.0315f0d2.chunk.js"
+  },
+  {
+    "revision": "a2f10b2e2d0f4c040449",
+    "url": "/image-compressor/static/js/runtime-main.9014142f.js"
+  }
+]);
